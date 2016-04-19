2ed3b16749da0fb4d1de09deb10b0281bbc583a6
anvaka <anvaka@gmail.com>
2016-04-18 22:17:15 -0700

    Allow isolated nodes
    Fixes https://github.com/anvaka/ngraph.louvain/issues/1

--- test/index.js
+++ test/index.js
@@ -1,16 +1,32 @@
 var modularity = require('../');
-var createGraph = require('miserables').create;
+var createMiseablesGraph = require('miserables').create;
+var createGraph = require('ngraph.graph');
 var test = require('tap').test;
 
 test('it can compute modularity for miserables graph', function(t) {
-  var graph = createGraph();
+  var graph = createMiseablesGraph();
   var community = modularity(graph);
 
   graph.forEachNode(function(node) {
-    t.ok(community.getClass(node.id), 'Node ' + node.id + ' has a defined community');
+    t.ok(community.getClass(node.id) !== undefined, 'Node ' + node.id + ' has a defined community');
   })
 
   t.ok(community.canCoarse(), 'Miserables can be further coarsened');
+  t.ok(community.originalModularity < community.newModularity, 'Modularity improved')
+
+  t.end();
+});
+
+test('it can compute modularity for graphs with isolated nodes', function(t) {
+  var graph = createGraph();
+  graph.addNode(1); // 1 is isolated
+  graph.addLink(2, 3);
+  var community = modularity(graph);
+
+  graph.forEachNode(function(node) {
+    t.ok(community.getClass(node.id) !== undefined, 'Node ' + node.id + ' has a defined community');
+  })
+
   t.ok(community.originalModularity < community.newModularity, 'Modularity improved')
 
   t.end();
