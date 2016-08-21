31e621af44f496421e19bac2c6696a87b855403c
anvaka <anvaka@gmail.com>
2016-08-21 00:16:39 -0700

    Show how to coarsen

--- demo/offline/index.js
+++ demo/offline/index.js
@@ -6,3 +6,15 @@ var clusters = detectClusters(graph);
 graph.forEachNode(function(node) {
   console.log('Node ' + node.id + ' belongs to ' + clusters.getClass(node.id));
 });
+
+var coarsen = require('ngraph.coarsen');
+
+while(clusters.canCoarse()) {
+  graph = coarsen(graph, clusters);
+  debugger;
+  clusters = detectClusters(graph);
+
+  graph.forEachNode(function(node) {
+    console.log('Node ' + node.id + ' belongs to ' + clusters.getClass(node.id));
+  });
+}
